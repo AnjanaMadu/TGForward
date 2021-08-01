0cad07d7f68a9593a51839924b304ec1a30309b1
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-08-01 19:33:06 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -1,7 +1,18 @@
-# ::: WARNING :::
-# THIS IS NOT A KANGED SCRIPT AND THIS IS FULLY CODED BY @ANJANAMADU.
-# @AmarnathCJD HE IS HELP ME TO MAKE THIS SCRIPT. 
-# ALRIGHTS RESERVED.
+'''TGForward, An Telegram Bot Project
+Copyright (c) 2021 Anjana Madu and Amarnath CDJ <https://github.com/AnjanaMadu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>'''
 
 import asyncio, sys
 from telethon import TelegramClient
